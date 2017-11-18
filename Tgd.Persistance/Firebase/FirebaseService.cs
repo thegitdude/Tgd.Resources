68412e372d492dfe8f67963eed15080ec863f108
thegitdude <andrei.git@salaryfinance.com>
2017-11-18 16:12:54 +0000

    implemented interface in firebaseService

--- Tgd.Persistance/Firebase/FirebaseService.cs
+++ Tgd.Persistance/Firebase/FirebaseService.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Tgd.Persistance.Model;
 
 namespace Tgd.Persistance.Firebase
 {
-    class FirebaseService
+    public class FirebaseService : IFirebaseService
     {
+        public Task<int> ExecuteAsync<T>(FirebasePersistanceTask<T> task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tout>> QueryAsync<Tin, Tout>(FirebasePersistanceTask<Tin> task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tout> QuerySingleAsync<Tin, Tout>(FirebasePersistanceTask<Tin> task)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
